a9c90f6ec684cf878b1a79bfe3dacdcc25fe74fc
TheUnderscoreWizard <70604751+TheUnderscoreWizard@users.noreply.github.com>
2021-05-26 07:45:36 -0400

    Add files via upload

---
+++ atm.py.py
@@ -0,0 +1,11 @@
+class Atm(object):
+    def __init__(self,cardNumber,pinNumber,company,speed_limit):
+        self.cardNumber=cardNumber
+        self.pinNumber=pinNumber
+
+def start(self):
+    print("CashWithdrawl")
+
+def stop(self):
+    print("BalanceEnquiry")
+
